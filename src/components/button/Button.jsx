0f04584b79760cd5a8e86137ab7261e00fe9a803
Dima <example.gmail.com>
2026-01-19 18:19:02 +0300

    debugging-3-done

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,7 +2,7 @@ import style from './button.module.css'
 
 export const Button = ({ text }) => {
     return (
-        <button className={`body-md-semibold ` + style.button}>
+        <button type='button' className={`body-md-semibold ` + style.button}>
             <img src="/plus.svg" alt="создать" />
             {text}
         </button>
